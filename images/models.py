f7f876dd8aca5e173c9976ff4c39f2fdffa2ef81
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-08-26 03:24:20 +0300

    Create models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,36 @@
+import uuid
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField()
+    annotation = models.OneToOneField(
+        'images.Annotation',
+        on_delete=models.DO_NOTHING,
+        blank=True,
+        null=True,
+    )
+
+
+class Annotation(models.Model):
+    labels = models.ManyToManyField(
+        'images.Label',
+        blank=True,
+    )
+
+
+LABEL_CLASS_ID_CHOICES = ('tooth', 'gum', 'lip')
+
+class Label(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+    )
+    class_id = models.TextField(
+        choices=[(item, item) for item in LABEL_CLASS_ID_CHOICES],
+    )
+    surface = ArrayField(models.CharField(max_length=1))
+    shape = ArrayField(models.FloatField(), size=4)
