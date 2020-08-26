20a2da6417b470d1e2bb70dcebe42f62b840c48b
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-08-26 16:57:24 +0300

    Annotation detail route

--- images/views.py
+++ images/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from . import models
 from . import serializers
@@ -9,3 +12,20 @@ class ImageViewSet(viewsets.ModelViewSet):
     queryset = models.Image.objects.all()
     serializer_class = serializers.ImageSerializer
     permission_classes = []
+
+    @action(detail=True, methods=['get', 'post'])
+    def annotation(self, request, pk):
+        image = self.get_object()
+        if request.method == 'GET':
+            return Response(
+                serializers.AnnotationSerializer(image.annotation).data
+            )
+        else:
+            serializer = serializers.AnnotationSerializer(data=request.data)
+            if serializer.is_valid():
+                return Response(serializer.data)
+            else:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
