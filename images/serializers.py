a96dee2ac7a7d27b657a69ac03366c2915bd600a
Dmitry Orlov <feanarokurufinve@gmail.com>
2020-08-27 13:17:18 +0300

    ShapeField

--- images/serializers.py
+++ images/serializers.py
@@ -3,18 +3,30 @@ from rest_framework import serializers
 from . import models
 
 
-class ExternalLabelSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = models.Label
-        fields = [
-            'id',
-            'class_id',
-            'surface',
+class ShapeField(serializers.Field):
+    """
+    Shape objects are serialized into dict notation.
+    """
+
+    def to_representation(self, value):
+        return {
+            'endX': value[0],
+            'endY': value[1],
+            'startX': value[2],
+            'startY': value[3]
+        }
+
+    def to_internal_value(self, data):
+        return [
+            data['endX'],
+            data['endY'],
+            data['startX'],
+            data['startY'],
         ]
 
 
 class LabelSerializer(serializers.ModelSerializer):
+    shape = ShapeField()
 
     def get_fields(self):
         base_fields = super().get_fields()
